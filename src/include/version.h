e694110cec7974709c3f7befe7f39fe92a269e2a
stainless <stainless>
2006-01-16 19:06:58 +0000

    Updated version number (correctly this time).

--- src/include/version.h
+++ src/include/version.h
@@ -1,36 +1,24 @@
+/* FakeNES - A free, portable, Open Source NES emulator.
+   Distributed under the Clarified Artistic License.
 
+   version.h: Global version definitions.
 
-/*
-
-FakeNES - A portable, Open Source NES emulator.
-
-Distributed under the Clarified Artistic License.
-
-version.h: Global version definitions.
-
-Copyright (c) 2001-2006, Randy McDowell.
-Copyright (c) 2001-2006, Charles Bilyue'.
-
-This is free software.  See 'LICENSE' for details.
-You must read and accept the license prior to use.
-
-*/
-
+   Copyright (c) 2001-2006, FakeNES Team.
+   This is free software.  See 'LICENSE' for details.
+   You must read and accept the license prior to use. */
 
 #ifndef VERSION_H_INCLUDED
-
 #define VERSION_H_INCLUDED
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define VERSION_MAJOR   0
+#define VERSION_MINOR   51
+#define VERSION         0x051
+#define VERSION_STRING  "0.5.1 (CVS)"
 
-#define VERSION_MINOR   40
-
-
-#define VERSION         0x040
-
-
-#define VERSION_STRING  "0.4.0 (CVS)"
-
-
-#endif /* ! VERSION_H_INCLUDED */
+#ifdef __cplusplus
+}
+#endif
+#endif   /* !VERSION_H_INCLUDED */
