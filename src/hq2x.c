4fa4963ee9bfda565bced3eaa0d8576ad8ca311d
stainless <stainless>
2006-03-06 22:44:26 +0000

    Included 'stdlib.h' to quell missing declaration warnings for 'abs'.

--- src/hq2x.c
+++ src/hq2x.c
@@ -16,6 +16,7 @@
 //License along with this program; if not, write to the Free Software
 //Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
+#include <stdlib.h>
 #include "common.h"
 
 typedef char bool; // from C++ to C
