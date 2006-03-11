6f1092f275e108f11a2049596ec7f390c6df2c0d
stainless <stainless>
2006-03-11 06:32:12 +0000

    Cleaned up ROM loading code, made ROM filenames Unicode compliant.

--- src/include/rom.h
+++ src/include/rom.h
@@ -15,151 +15,81 @@
 extern "C" {
 #endif
 
-
 typedef struct _ROM ROM;
 
+/* We include 'mmc.h' (after the declaration of 'ROM') here instead of above
+   to avoid a mutual-inclusion conflict. */
 #include "mmc.h"
 
 ROM global_rom;
-
-
-int rom_is_loaded;
-
-
-#define ROM_TRAINER_SIZE    512
-
-
-#define ROM_CTRL_MIRRORING      0x01
-
-#define ROM_CTRL_BATTERY        0x02
-
-#define ROM_CTRL_TRAINER        0x04
-
-#define ROM_CTRL_FOUR_SCREEN    0x08
-
-
+BOOL rom_is_loaded;
+    
 struct _ROM
 {
-    const MMC * current_mmc;            /* Memory mapping controller. */
-
-
-    UINT8 * trainer;
-
-    UINT32 trainer_crc32;               /* Checksum for trainer. */
-
-
-    UINT8 * prg_rom;
-
-    UINT32 prg_rom_crc32;               /* Checksum for PRG ROM. */
-
-
-    UINT8 * chr_rom;
-
-    UINT32 chr_rom_crc32;                /* Checksum for CHR ROM. */
-
-
-    UINT8 * chr_rom_cache;              /* CHR-ROM cache buffer. */
-
-    UINT8 * chr_rom_cache_tag;          /* CHR-ROM cache tag buffer. */
-
-
-    int prg_rom_pages;
-
-    int chr_rom_pages;
-
-
-    UINT8 control_byte_1;
-
-    UINT8 control_byte_2;
-
-
-    int mapper_number;
-
-                           
-    UINT8 chr_rom_page_overflow_mask;   /* CHR-ROM bank # wrapping mask. */
-
-    UINT8 chr_rom_page_lookup [256];
-
-
-    UINT8 prg_rom_page_overflow_mask;   /* PRG-ROM bank # wrapping mask. */
-
-    UINT8 prg_rom_page_lookup [256];
-
-
-    int sram_flag;
-
-
-    UINT8 filename [256];
+   const MMC *current_mmc;             /* Memory mapping controller. */
+   UINT8 *trainer;                     /* Pointer to trainer data. */
+   UINT8 *prg_rom;                     /* Pointer to PRG-ROM data. */
+   UINT8 *chr_rom;                     /* Pointer to CHR-ROM data. */
+   UINT32 trainer_crc32;               /* Checksum for trainer. */
+   UINT32 prg_rom_crc32;               /* Checksum for PRG-ROM. */
+   UINT32 chr_rom_crc32;               /* Checksum for CHR-ROM. */
+   UINT8 *chr_rom_cache;               /* CHR-ROM cache buffer. */
+   UINT8 *chr_rom_cache_tag;           /* CHR-ROM cache tag buffer. */
+   int prg_rom_pages;                  /* Number of PRG-ROM pages. */
+   int chr_rom_pages;                  /* Nimber of CHR-ROM pages. */
+   UINT8 control_byte_1;               /* Header control byte #1. */
+   UINT8 control_byte_2;               /* Header control byte #2. */
+   int mapper_number;                  /* Number of MMC/mapper to use. */
+   UINT8 chr_rom_page_overflow_mask;   /* CHR-ROM bank # wrapping mask. */
+   UINT8 chr_rom_page_lookup[256];     /* ?? */
+   UINT8 prg_rom_page_overflow_mask;   /* PRG-ROM bank # wrapping mask. */
+   UINT8 prg_rom_page_lookup[256];     /* ?? */
+   BOOL sram_flag;                     /* If Save RAM/SRAM is present. */
+   USTRING filename;                   /* Full Unicode filename. */
 };
-
 
 typedef struct _INES_HEADER
 {
-    UINT8 signature [4];
-
-
-    UINT8 prg_rom_pages;
-
-    UINT8 chr_rom_pages;
-
-
-    UINT8 control_byte_1;
-
-    UINT8 control_byte_2;
-
-
-    UINT8 reserved [8];
+   UINT8 signature[4];
+   UINT8 prg_rom_pages;
+   UINT8 chr_rom_pages;
+   UINT8 control_byte_1;
+   UINT8 control_byte_2;
+   UINT8 reserved[8];
 
 } INES_HEADER;
 
-
-int load_rom (const UINT8 *, ROM *);
-
-int load_rom_from_zip (const UINT8 *, ROM *);
-
-
+int load_rom (const UCHAR*, ROM *);
+int load_rom_from_zip (const UCHAR*, ROM *);
 void free_rom (const ROM *);
 
+#define ROM_TRAINER_SIZE      512
 
-#define ROM_PRG_ROM_PAGES   global_rom.prg_rom_pages
+/* Control byte flags. */
+#define ROM_CTRL_MIRRORING    (1 << 0)
+#define ROM_CTRL_BATTERY      (1 << 1)
+#define ROM_CTRL_TRAINER      (1 << 2)
+#define ROM_CTRL_FOUR_SCREEN  (1 << 2)
 
-#define ROM_CHR_ROM_PAGES   global_rom.chr_rom_pages
+/* Shortcut macros. */
+#define ROM_PRG_ROM_PAGES              global_rom.prg_rom_pages
+#define ROM_CHR_ROM_PAGES              global_rom.chr_rom_pages
+#define ROM_MAPPER_NUMBER              global_rom.mapper_number
+#define ROM_CURRENT_MMC                global_rom.current_mmc
+#define ROM_PRG_ROM                    global_rom.prg_rom
+#define ROM_CHR_ROM                    global_rom.chr_rom
+#define ROM_CHR_ROM_CACHE              global_rom.chr_rom_cache
+#define ROM_CHR_ROM_CACHE_TAG          global_rom.chr_rom_cache_tag
+#define ROM_PRG_ROM_PAGE_LOOKUP        global_rom.prg_rom_page_lookup
+#define ROM_PRG_ROM_PAGE_OVERFLOW_MASK global_rom.prg_rom_page_overflow_mask
+#define ROM_CHR_ROM_PAGE_LOOKUP        global_rom.chr_rom_page_lookup
+#define ROM_CHR_ROM_PAGE_OVERFLOW_MASK global_rom.chr_rom_page_overflow_mask
 
-
-#define ROM_MAPPER_NUMBER   global_rom.mapper_number
-
-#define ROM_CURRENT_MMC     global_rom.current_mmc
-
-
-#define ROM_PRG_ROM         global_rom.prg_rom
-
-#define ROM_CHR_ROM         global_rom.chr_rom
-
-
-#define ROM_CHR_ROM_CACHE       global_rom.chr_rom_cache
-
-#define ROM_CHR_ROM_CACHE_TAG   global_rom.chr_rom_cache_tag
-
-
-#define ROM_PRG_ROM_PAGE_LOOKUP             global_rom.prg_rom_page_lookup
-
-#define ROM_PRG_ROM_PAGE_OVERFLOW_MASK      global_rom.prg_rom_page_overflow_mask
-
-
-#define ROM_CHR_ROM_PAGE_LOOKUP             global_rom.chr_rom_page_lookup
-
-#define ROM_CHR_ROM_PAGE_OVERFLOW_MASK      global_rom.chr_rom_page_overflow_mask
-
-
-#define ROM_PAGE_16K(index)     (ROM_PRG_ROM + (index) * 0x4000)
-
-#define ROM_PAGE_8K(index)      (ROM_PRG_ROM + (index) * 0x2000)
-
-
-#define FIRST_ROM_PAGE  ROM_PRG_ROM
-
-#define LAST_ROM_PAGE   (ROM_PAGE_16K (ROM_PRG_ROM_PAGES - 1))
-
+/* Memory map helper macros. */
+#define ROM_PAGE_16K(page) (ROM_PRG_ROM + (page * 0x4000))
+#define ROM_PAGE_8K(page)  (ROM_PRG_ROM + (page * 0x2000))
+#define FIRST_ROM_PAGE     ROM_PRG_ROM
+#define LAST_ROM_PAGE      (ROM_PAGE_16K((ROM_PRG_ROM_PAGES - 1)))
 
 #ifdef __cplusplus
 }
--- src/rom.c
+++ src/rom.c
@@ -7,6 +7,7 @@
    This is free software.  See 'LICENSE' for details.
    You must read and accept the license prior to use. */
 
+#include <allegro.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,426 +21,258 @@
 #include "unzip.h"
 #endif
 
-int rom_is_loaded = FALSE;
-
+BOOL rom_is_loaded = FALSE;
 
 #ifdef USE_ZLIB
+#  define LR_FILE                     gzFile
+#  define LR_OPEN(name, mode)         (gzopen (name, mode "b"))
+#  define LR_CLOSE(file)              (gzclose (file))
+#  define LR_READ(file, buffer, size) (gzread (file, buffer, size))
+#else /* USE_ZLIB */
+#  define LR_FILE                     PACKFILE *
+#  define LR_OPEN(name, mode)         (pack_fopen (name, mode))
+#  define LR_CLOSE(file)              (pack_fclose (file))
+#  define LR_READ(file, buffer, size) (pack_fread (buffer, size, file))
+#endif   /* !USE_ZLIB */
 
-
-#define LR_FILE     gzFile
-
-
-#define LR_OPEN(name, mode)     (gzopen (name, mode))
-
-#define LR_CLOSE(file)          (gzclose (file))
-
-
-#define LR_READ(file, buffer, size)     (gzread (file, buffer, size))
-
-
-#else
-
-
-#define LR_FILE     FILE *
-
-
-#define LR_OPEN(name, mode)     (fopen (name, mode))
-
-#define LR_CLOSE(file)          (fclose (file))
-
-
-#define LR_READ(file, buffer, size)     (fread (buffer, 1, size, file))
-
-
-#endif /* USE_ZLIB */
-
-
-int load_rom (const UINT8 * filename, ROM * rom)
+int load_rom (const UCHAR *filename, ROM *rom)
 {
-    INES_HEADER ines_header;
-
-    LR_FILE rom_file;
-
-
-    UINT8 test;
-
-
-    /* Initialize the ROM context. */
-
-    memset (rom, NIL, sizeof (ROM));
-
-
-    /* Check if ROM is inside a ZIP file. */
-
-    if ((strncmp (get_extension (filename), "zip", 3) == 0) ||
-        (strncmp (get_extension (filename), "ZIP", 3) == 0))
-    {
-        return (load_rom_from_zip (filename, rom));
-    }
-
-
-    /* Open the file. */
-
-    rom_file = LR_OPEN (filename, "rb");
-
-    if (! rom_file)
-    {
-        return (1);
-    }
-
-
-    /* Read the header. */
-
-    LR_READ (rom_file, &ines_header, sizeof (INES_HEADER));
-
-
-    /* Verify the signature. */
-
-    if (strncmp ((char *) ines_header.signature, "NES\x1a", 4))
-    {
-        LR_CLOSE (rom_file);
-    
-        return (1);
-    }
-
-
-    /* Verify that PRG-ROM exists. */
-
-    if (ines_header.prg_rom_pages == 0)
-    {
-        LR_CLOSE (rom_file);
-
-        return (1);
-    }
-
-
-    /* Check for 'DiskDude!'. */
-
-    if ((ines_header.control_byte_2 == 'D') &&
-        (! (strncmp ((char *) ines_header.reserved, "iskDude!", 8))))
-    {
-        ines_header.control_byte_2 = 0;
-    }
-
-
-    /* Read page/bank count. */
-
-    rom -> prg_rom_pages = ines_header.prg_rom_pages;
-
-    rom -> chr_rom_pages = ines_header.chr_rom_pages;
-
-
-    /* Read control bytes. */
-
-    rom -> control_byte_1 = ines_header.control_byte_1;
-
-    rom -> control_byte_2 = ines_header.control_byte_2;
-
-
-    /* Derive mapper number. */
-
-    rom -> mapper_number = ((rom -> control_byte_2 & 0xf0) | ((rom -> control_byte_1 & 0xf0) >> 4));
-
-
-    mmc_request (rom);
-
-
-    /* Allocate and load trainer. */
-
-    if ((rom -> control_byte_1 & ROM_CTRL_TRAINER))
-    {
-        rom -> trainer = malloc (ROM_TRAINER_SIZE);
-
-        if (! rom -> trainer)
-        {
-            LR_CLOSE (rom_file);
-
-            return (1);
-        }
-
-
-        /* initialize area not present in image to a known value */
-        memset (rom -> trainer, 0xFF, ROM_TRAINER_SIZE);
-
-        /* read in trainer */
-        LR_READ (rom_file, rom -> trainer, ROM_TRAINER_SIZE);
-    }
-
-
-    /* Allocate and load PRG-ROM. */
-
-    if (! cpu_get_prg_rom_pages (rom))
-    {
-        free_rom (rom);
-
-        LR_CLOSE (rom_file);
-
-
-        return (1);
-    }
-
-
-    /* read in PRG ROM */
-    LR_READ (rom_file, rom -> prg_rom, (rom -> prg_rom_pages * 0x4000));
-
-
-    /* Allocate and load CHR-ROM. */
-
-    if (rom -> chr_rom_pages)
-    {
-        if (! ppu_get_chr_rom_pages (rom))
-        {
-            free_rom (rom);
-
-            LR_CLOSE (rom_file);
-
-
-            return (1);
-        }
-
-        /* read in CHR ROM */
-        LR_READ (rom_file, rom -> chr_rom, (rom -> chr_rom_pages * 0x2000));
-    }
-
-
-    /* Fill in extra stuff. */
-
-    append_filename (rom -> filename, "", filename, sizeof (rom -> filename));
-
-    rom -> sram_flag = (rom -> control_byte_1 & ROM_CTRL_BATTERY);
-
-
-    if ((rom -> control_byte_1 & ROM_CTRL_FOUR_SCREEN))
-    {
+   INES_HEADER header;
+   LR_FILE file;
+   UINT8 test;
+
+   /* Initialize the ROM context. */
+   memset (rom, NULL, sizeof (ROM));
+
+   /* Check if ROM is inside a ZIP file. */
+   if (ustrnicmp (get_extension (filename), "zip", USTRING_SIZE) == 0)
+      return (load_rom_from_zip (filename, rom));
+
+   /* Open the file. */
+   file = LR_OPEN (filename, "r");
+   if (!file)
+      return (1);
+
+   /* Read the header. */
+   LR_READ(file, &header, sizeof (INES_HEADER));
+
+   /* Verify the signature. */
+   if (strncmp ((char *)header.signature, "NES\x1a", 4))
+   {
+      /* Verification failed. */
+      LR_CLOSE(file);
+      return (1);
+   }
+
+   /* Verify that PRG-ROM exists. */
+   if (header.prg_rom_pages == 0)
+   {
+      /* No code to run. ;) */
+      LR_CLOSE(file);
+      return (2);
+   }
+
+   /* Check for 'DiskDude!' contamination. */
+   if ((header.control_byte_2 == 'D') &&
+       (!(strncmp ((char *)header.reserved, "iskDude!", 8))))
+   {
+      /* Clean header. */
+      header.control_byte_2 = 0;
+   }
+
+   /* Read page/bank count. */
+   rom->prg_rom_pages = header.prg_rom_pages;
+   rom->chr_rom_pages = header.chr_rom_pages;
+
+   /* Read control bytes. */
+   rom->control_byte_1 = header.control_byte_1;
+   rom->control_byte_2 = header.control_byte_2;
+
+   /* Derive mapper number. */
+   rom->mapper_number = ((rom->control_byte_2 & 0xf0) |
+      ((rom->control_byte_1 & 0xf0) >> 4));
+
+   /* Set mapper. */
+   mmc_request (rom);
+
+   /* Load trainer. */
+   if ((rom->control_byte_1 & ROM_CTRL_TRAINER))
+   {
+      rom->trainer = malloc (ROM_TRAINER_SIZE);
+      if (!rom->trainer)
+      {
+         LR_CLOSE(file);
+         return (3);
+      }
+
+      LR_READ(file, rom->trainer, ROM_TRAINER_SIZE);
+   }
+
+   /* Load PRG-ROM. */
+   if (!cpu_get_prg_rom_pages (rom))
+   {
+      free_rom (rom);
+      LR_CLOSE(file);
+      return (4);
+   }
+
+   LR_READ(file, rom->prg_rom, (rom->prg_rom_pages * 0x4000));
+
+   /* Load CHR-ROM. */
+   if (rom->chr_rom_pages > 0)
+   {
+      if (!ppu_get_chr_rom_pages (rom))
+      {
+         free_rom (rom);
+         LR_CLOSE(file);
+         return (5);
+      }
+
+      LR_READ(file, rom->chr_rom, (rom->chr_rom_pages * 0x2000));
+   }
+
+   /* Close the file. */
+   LR_CLOSE (file);
+
+   /* Fill in filename. */
+   append_filename (rom->filename, "", filename, sizeof (rom->filename));
+
+   /* Copy SRAM flag. */
+   rom->sram_flag = (rom->control_byte_1 & ROM_CTRL_BATTERY);
+
+   /* Set mirroring. */
+   if ((rom->control_byte_1 & ROM_CTRL_FOUR_SCREEN))
+   {
         ppu_set_mirroring (MIRRORING_FOUR_SCREEN);
-    }
-    else
-    {
-        ppu_set_mirroring (((rom -> control_byte_1 & ROM_CTRL_MIRRORING)
-            ? MIRRORING_VERTICAL : MIRRORING_HORIZONTAL));
-    }
+   }
+   else
+   {
+      ppu_set_mirroring (((rom->control_byte_1 & ROM_CTRL_MIRRORING) ?
+         MIRRORING_VERTICAL : MIRRORING_HORIZONTAL));
+   }
 
-
-    /* Close the file. */
-
-    LR_CLOSE (rom_file);
-
-
-    return (0);
+   return (0);
 }
 
-
-int load_rom_from_zip (const UINT8 * filename, ROM * rom)
+int load_rom_from_zip (const UCHAR *filename, ROM *rom)
 {
-    INES_HEADER ines_header;
-
+    INES_HEADER header;
 
 #ifdef USE_ZLIB
-
-
-    unzFile zip_file;
-
-
-    unz_file_info unused;
-
-
-    UINT8 test;
-
-
-    /* Open the ZIP file. */
-
-    zip_file = unzOpen (filename);
-
-    if (! zip_file)
-    {
-        return (1);
-    }
-
-
-    /* Open the first file in the ZIP file. */
-
-    unzGoToFirstFile (zip_file);
-
-    unzOpenCurrentFile (zip_file);
-
-
-    /* Read the header. */
-
-    unzReadCurrentFile (zip_file, &ines_header, sizeof (INES_HEADER));
-
-
-    /* Verify the signature. */
-
-    if (strncmp ((char *) ines_header.signature, "NES\x1a", 4))
-    {
-        unzCloseCurrentFile (zip_file);
-
-        unzClose (zip_file);
-
-
-        return (1);
-    }
-
-
-    /* Verify that PRG-ROM exists. */
-
-    if (ines_header.prg_rom_pages == 0)
-    {
-        unzCloseCurrentFile (zip_file);
-
-        unzClose (zip_file);
-
-
-        return (1);
-    }
-
-
-    /* Check for 'DiskDude!'. */
-
-    if ((ines_header.control_byte_2 == 'D') &&
-        (! (strncmp ((char *) ines_header.reserved, "iskDude!", 8))))
-    {
-        ines_header.control_byte_2 = 0;
-    }
-
-
-    /* Read page/bank count. */
-
-    rom -> prg_rom_pages = ines_header.prg_rom_pages;
-
-    rom -> chr_rom_pages = ines_header.chr_rom_pages;
-
-
-    /* Read control bytes. */
-
-    rom -> control_byte_1 = ines_header.control_byte_1;
-
-    rom -> control_byte_2 = ines_header.control_byte_2;
-
-
-    /* Derive mapper number. */
-
-    rom -> mapper_number = ((rom -> control_byte_2 & 0xf0) | ((rom -> control_byte_1 & 0xf0) >> 4));
-
-
-    mmc_request (rom);
-
-
-    /* Allocate and load trainer. */
-
-    if ((rom -> control_byte_1 & ROM_CTRL_TRAINER))
-    {
-        rom -> trainer = malloc (ROM_TRAINER_SIZE);
-
-        if (! rom -> trainer)
-        {
-            unzCloseCurrentFile (zip_file);
-
-            unzClose (zip_file);
-
-
-            return (1);
-        }
-
-
-        /* initialize area not present in image to a known value */
-        memset (rom -> trainer, 0xFF, ROM_TRAINER_SIZE);
-
-        /* read in trainer */
-        unzReadCurrentFile (zip_file, rom -> trainer, ROM_TRAINER_SIZE);
-    }
-
-
-    /* Allocate and load PRG-ROM. */
-
-    if (! cpu_get_prg_rom_pages (rom))
-    {
-        free_rom (rom);
-
-
-        unzCloseCurrentFile (zip_file);
-
-        unzClose (zip_file);
-
-
-        return (1);
-    }
-
-
-    /* read in PRG ROM */
-    unzReadCurrentFile (zip_file, rom -> prg_rom, (rom -> prg_rom_pages * 0x4000));
-
-
-    /* Allocate and load CHR-ROM. */
-
-    if (rom -> chr_rom_pages)
-    {
-        if (! ppu_get_chr_rom_pages (rom))
-        {
-            free_rom (rom);
-
-
-            unzCloseCurrentFile (zip_file);
-
-            unzClose (zip_file);
-
-
-            return (1);
-        }
-
-
-        /* read in CHR ROM */
-        unzReadCurrentFile (zip_file, rom -> chr_rom, (rom -> chr_rom_pages * 0x2000));
-    }
-
-
-    /* Fill in extra stuff. */
-
-    unzGetCurrentFileInfo (zip_file, &unused, rom -> filename, sizeof (rom -> filename), NIL, NIL, NIL, NIL);
-
-    rom -> sram_flag = (rom -> control_byte_1 & ROM_CTRL_BATTERY);
-
-
-    if ((rom -> control_byte_1 & ROM_CTRL_FOUR_SCREEN))
-    {
-        ppu_set_mirroring (MIRRORING_FOUR_SCREEN);
-    }
-    else
-    {
-        ppu_set_mirroring (((rom -> control_byte_1 & ROM_CTRL_MIRRORING)
-            ? MIRRORING_VERTICAL : MIRRORING_HORIZONTAL));
-    }
-
-
-    /* Close the file. */
-
-    unzCloseCurrentFile (zip_file);
-
-    unzClose (zip_file);
-
-
-    return (0);
-
+   unzFile file;
+   unz_file_info unused;
+   UINT8 test;
+
+   /* Open the ZIP file. */
+   file = unzOpen (filename);
+   if (!file)
+      return (1);
+
+   /* Open the first file in the ZIP file. */
+   unzGoToFirstFile (file);
+   unzOpenCurrentFile (file);
+
+   unzReadCurrentFile (file, &header, sizeof (INES_HEADER));
+
+   if (strncmp ((char *)header.signature, "NES\x1a", 4))
+   {
+      unzCloseCurrentFile (file);
+      unzClose (file);
+      return (2);
+   }
+
+   if (header.prg_rom_pages == 0)
+   {
+      unzCloseCurrentFile (file);
+      unzClose (file);
+      return (3);
+   }
+
+   if ((header.control_byte_2 == 'D') &&
+       (!(strncmp ((char *)header.reserved, "iskDude!", 8))))
+   {
+      header.control_byte_2 = 0;
+   }
+
+   rom->prg_rom_pages  = header.prg_rom_pages;
+   rom->chr_rom_pages  = header.chr_rom_pages;
+   rom->control_byte_1 = header.control_byte_1;
+   rom->control_byte_2 = header.control_byte_2;
+
+   rom->mapper_number = ((rom->control_byte_2 & 0xf0) |
+      ((rom->control_byte_1 & 0xf0) >> 4));
+   mmc_request (rom);
+
+   if ((rom->control_byte_1 & ROM_CTRL_TRAINER))
+   {
+      rom->trainer = malloc (ROM_TRAINER_SIZE);
+      if (!rom->trainer)
+      {
+         unzCloseCurrentFile (file);
+         unzClose (file);
+         return (4);
+      }
+
+      unzReadCurrentFile (file, rom->trainer, ROM_TRAINER_SIZE);
+   }
+
+   if (!cpu_get_prg_rom_pages (rom))
+   {
+      free_rom (rom);
+      unzCloseCurrentFile (file);
+      unzClose (file);
+      return (5);
+   }
+
+   unzReadCurrentFile (file, rom->prg_rom, (rom->prg_rom_pages * 0x4000));
+
+   if (rom->chr_rom_pages > 0)
+   {
+      if (!ppu_get_chr_rom_pages (rom))
+      {
+         free_rom (rom);
+         unzCloseCurrentFile (file);
+         unzClose (file);
+         return (6);
+      }
+
+      unzReadCurrentFile (file, rom->chr_rom, (rom->chr_rom_pages *
+         0x2000));
+   }
+
+   unzGetCurrentFileInfo (file, &unused, rom->filename, sizeof
+      (rom->filename), NULL, NULL, NULL, NULL);
+
+   /* Close the file. */
+   unzCloseCurrentFile (file);
+   unzClose (file);
+
+   rom->sram_flag = (rom->control_byte_1 & ROM_CTRL_BATTERY);
+
+   if ((rom->control_byte_1 & ROM_CTRL_FOUR_SCREEN))
+   {
+      ppu_set_mirroring (MIRRORING_FOUR_SCREEN);
+   }
+   else
+   {
+      ppu_set_mirroring (((rom->control_byte_1 & ROM_CTRL_MIRRORING) ?
+         MIRRORING_VERTICAL : MIRRORING_HORIZONTAL));
+   }
+
+   return (0);
 
 #else /* USE_ZLIB */
 
-
-    return (1);
-
+   return (7);
 
 #endif
 }
 
-
-void free_rom (const ROM * rom)
+void free_rom (const ROM *rom)
 {
-    if (rom -> trainer)
-    {
-        free (rom -> trainer);
-    }
+   if (rom->trainer)
+      free (rom->trainer);
 
-
-    cpu_free_prg_rom (rom);
-
-    ppu_free_chr_rom (rom);
+   cpu_free_prg_rom (rom);
+   ppu_free_chr_rom (rom);
 }
