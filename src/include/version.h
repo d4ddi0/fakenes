731254e4266178c70855773490c4e75b20f89348
stainless <stainless>
2006-03-28 18:36:37 +0000

    Incremented version number.

--- src/include/version.h
+++ src/include/version.h
@@ -14,15 +14,15 @@ extern "C" {
 #endif
 
 #define VERSION_MAJOR   0
-#define VERSION_MINOR   53
-#define VERSION         0x053
+#define VERSION_MINOR   54
+#define VERSION         0x054
 
 /* Version tag can be overriden by Makefiles. */
 #ifndef VERSION_TAG
 #define VERSION_TAG     "CVS"
 #endif
 
-#define VERSION_STRING  "0.5.3 (" VERSION_TAG ")"
+#define VERSION_STRING  "0.5.4 (" VERSION_TAG ")"
 
 #ifdef __cplusplus
 }
