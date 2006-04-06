517428615b4c40657b16ae533bb3030fb4b48cd0
stainless <stainless>
2006-04-06 06:24:48 +0000

    Split platform-specific initialization code into it's own file.

--- src/main.c
+++ src/main.c
@@ -11,27 +11,17 @@
 #include <allegro.h>
 #include "build.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#ifdef POSIX
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <errno.h>
-extern int errno;
-#endif
 #include "audio.h"
 #include "common.h"
 #include "cpu.h"
 #include "data.h"
 #include "gui.h"
 #include "input.h"
-#include "log.h"
 #include "mmc.h"
 #include "net.h"
 #include "netplay.h"
 #include "papu.h"
+#include "platform.h"
 #include "ppu.h"
 #include "rom.h"
 #include "timing.h"
@@ -70,23 +60,6 @@ int timing_audio_hertz = 0;
 
 
 BOOL timing_half_speed = FALSE;
-
-
-#ifdef POSIX
-
-UINT8 * homedir = NIL;
-
-UINT8 * logdir = NIL;
-
-UINT8 * confdir = NIL;
-
-
-static DIR * tmpdir = NIL;
-
-#endif
-
-
-UINT8 logfile [256];
 
 
 static int redraw_flag = TRUE;
@@ -239,11 +212,6 @@ int main (int argc, char * argv [])
     int result;
 
 
-    UINT8 buffer [256];
-
-    UINT8 buffer2 [256];
-
-
     if (VERSION == 0x030)
     {
         printf ("\nThis release is dedicated to those who fell in the 9/11 attacks.\n");
@@ -280,237 +248,8 @@ int main (int argc, char * argv [])
     set_window_title ("FakeNES");
 
 
-#ifdef POSIX
-
-    /* by amit */
-
-
-    /* Configuration directory checking */
-
-    homedir = getenv ("HOME");
-
-
-    if (homedir)
-    {
-        const UINT8 confdir_base [] = "/.fakenes";
-
-
-        const UINT8 logdir_base [] = "/logs";
-
-
-        confdir = ((UINT8 *) malloc (strlen (homedir) + sizeof (confdir_base)));
-
-
-        if (confdir)
-        {
-            strcpy (confdir, homedir);
-
-            strcat (confdir, confdir_base);
-
-
-            logdir = ((UINT8 *) malloc (strlen (confdir) + sizeof (logdir_base)));
-
-
-            if (logdir)
-            {
-                strcpy (logdir, confdir);
-
-                strcat (logdir, logdir_base);
-
-
-                memset (logfile, NIL, sizeof (logfile));
-
-
-                strcat (logfile, logdir);
-
-                strcat (logfile, "/messages");
-            }
-        }
-        else
-        {
-            logdir = NIL;
-        }
-
-
-        if (! confdir)
-        {
-            fprintf (stderr, "Error when generating configuration path.\nConfiguration will not be saved.\n\n");
-        }
-        else
-        {
-            /* Just see if we can open the directory. */
-              
-            if (! (tmpdir = opendir (confdir)))
-            {
-                /* Directory doesn't exist, create it. */
-
-                if (errno == ENOENT)
-                {
-                    if (mkdir (confdir, (S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH)) == -1)
-                    {
-                        fprintf (stderr, "Error creating \"%s\".\nConfiguration will not be saved.\n\n", confdir);
-
-
-                        free (confdir);
-
-                        confdir = NIL;
-                    }
-                    else    /* mkdir was successful, make the log dir. */
-                    {
-                        /* Error checking for logdir happens later. */
-
-                        mkdir (logdir, (S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH));
-                    }
-                }
-                else
-                {
-                    UINT8 errorbuf [300] = { NIL };
-
-
-                    strcat (errorbuf, confdir);
-
-                    perror (errorbuf);
-
-
-                    fprintf (stderr, "%s.\nConfiguration will not be saved.\n\n", errorbuf);
-
-
-                    free (confdir);
-
-                    confdir = NIL;
-                }
-            }
-            else
-            {
-                /* Close the directory we just opened. */
-
-                closedir (tmpdir);
-            }
-        }
-
-    }
-    else
-    {
-        fprintf (stderr, "$HOME appears to not be set.\nConfiguration will not be saved.\n\n");
-    }
-
-
-    /* Load up the configuration file. */
-
-    if (confdir)
-    {
-        const UINT8 conffile_base [] = "/config";
-
-
-        UINT8 * conffile = malloc (strlen (confdir) + sizeof (conffile_base));
-
-
-        strcpy (conffile, confdir);
-
-        strcat (conffile, conffile_base);
-
-
-        set_config_file (conffile);
-    }
-    else
-    {
-        set_config_file ("/dev/null");
-    }
-
-
-    /* Check the logs directory. */
-
-    if (! logdir)
-    {
-        /* If we have a valid home directory, there was an error. */
-
-        if (homedir)
-        {
-            fprintf (stderr, "Error when generating log path.\nLogs will not be saved.\n\n");
-        }
-    }
-    else
-    {
-        if (! (tmpdir = opendir (logdir)))
-        {
-            if (errno == ENOENT)
-            {
-                if (mkdir (logdir, (S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH)) == -1)
-                {
-                    fprintf (stderr, "Error creating \"%s\"\n\n", logdir);
-
-
-                    free (logdir);
-
-                    logdir = NIL;
-                }
-            }
-            else
-            {
-                UINT8 errorbuf [300] = { NIL };
-
-
-                strcat (errorbuf, confdir);
-
-                perror (errorbuf);
-
-
-                fprintf (stderr, "%s.\nConfiguration will not be saved.\n\n", errorbuf);
-
-
-                free (confdir);
-
-                logdir = NIL;
-            }
-        }
-        else
-        {
-            /* Close the directory we just opened. */
-
-            closedir (tmpdir);
-        }
-    }
-
-
-    log_open (logfile);
-
-#else
-
-
-    memset (buffer, NIL, sizeof (buffer));
-
-    memset (buffer2, NIL, sizeof (buffer2));
-
-
-    get_executable_name (buffer, sizeof (buffer));
-
-
-#ifdef ALLEGRO_WINDOWS
-
-   replace_filename (buffer2, buffer, "fakenesw.cfg", sizeof (buffer2));
-
-#else
-
-   replace_filename (buffer2, buffer, "fakenes.cfg", sizeof (buffer2));
-
-#endif
-
-
-    set_config_file (buffer2);
-
-
-    replace_filename (buffer2, buffer, "messages.log", sizeof (buffer2));
-
-
-    log_open (buffer2);
-
-
-    memset (logfile, NIL, sizeof (logfile));
-
-
-    strcat (logfile, buffer2);
-
-#endif
+    if ((result = platform_init ()) != 0)
+        return ((8 + result));
 
 
     frame_skip_min = get_config_int ("timing", "frame_skip_min", 0);
@@ -1083,7 +822,7 @@ int main (int argc, char * argv [])
     input_exit ();
 
 
-    log_close ();
+    platform_exit ();
 
 
     if (rom_is_loaded)
