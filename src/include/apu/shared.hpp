0f4ddc9c5532a6cf526c3519377e81cc3948e8f2
randilyn <randilyn>
2006-10-01 06:05:05 +0000

    Piped through 'indent' to make it a bit less of a mess.

--- src/include/apu/shared.hpp
+++ src/include/apu/shared.hpp
@@ -9,63 +9,71 @@
 static UINT32 apu_lineartbl[(1 << APU_LIN_BITS) + 1];
 static UINT32 apu_logtbl[1 << APU_LOG_BITS];
 
-static UINT32 APU_LinearToLog(INT32 l)
+static UINT32
+APU_LinearToLog (INT32 l)
 {
-   return (l < 0) ? (apu_lineartbl[-l] + 1) : apu_lineartbl[l];
+  return (l < 0) ? (apu_lineartbl[-l] + 1) : apu_lineartbl[l];
 }
 
-static INT32 APU_LogToLinear(UINT32 l, UINT32 sft)
+static INT32
+APU_LogToLinear (UINT32 l, UINT32 sft)
 {
-    INT32 ret;
-    UINT32 ofs;
-   sft += (l >> 1) >> APU_LOG_BITS;
-   if (sft >= APU_LOG_LIN_BITS) return 0;
-   ofs = (l >> 1) & ((1 << APU_LOG_BITS) - 1);
-   ret = apu_logtbl[ofs] >> sft;
-	return (l & 1) ? -ret : ret;
+  INT32 ret;
+  UINT32 ofs;
+  sft += (l >> 1) >> APU_LOG_BITS;
+  if (sft >= APU_LOG_LIN_BITS)
+    return 0;
+  ofs = (l >> 1) & ((1 << APU_LOG_BITS) - 1);
+  ret = apu_logtbl[ofs] >> sft;
+  return (l & 1) ? -ret : ret;
 }
 
-static void APU_LogTableInitialize(void)
+static void
+APU_LogTableInitialize (void)
 {
-    static INT32 initialized = 0;
-    INT32 i;
-	double a;
-	if (initialized) return;
-	initialized = 1;
-   for (i = 0; i < (1 << APU_LOG_BITS); i++)
-	{
-      a = (1 << APU_LOG_LIN_BITS) / pow(2, i / (double)(1 << APU_LOG_BITS));
-        apu_logtbl[i] = (UINT32)a;
-	}
-   apu_lineartbl[0] = APU_LOG_LIN_BITS << APU_LOG_BITS;
-   for (i = 1; i < (1 << APU_LIN_BITS) + 1; i++)
-	{
-        UINT32 ua;
+  static INT32 initialized = 0;
+  INT32 i;
+  double a;
+  if (initialized)
+    return;
+  initialized = 1;
+  for (i = 0; i < (1 << APU_LOG_BITS); i++)
+    {
+      a = (1 << APU_LOG_LIN_BITS) / pow (2, i / (double) (1 << APU_LOG_BITS));
+      apu_logtbl[i] = (UINT32) a;
+    }
+  apu_lineartbl[0] = APU_LOG_LIN_BITS << APU_LOG_BITS;
+  for (i = 1; i < (1 << APU_LIN_BITS) + 1; i++)
+    {
+      UINT32 ua;
       a = i << (APU_LOG_LIN_BITS - APU_LIN_BITS);
-        ua = (UINT32)((APU_LOG_LIN_BITS - (log(a) / log(2))) * (1 << APU_LOG_BITS));
+      ua =
+	(UINT32) ((APU_LOG_LIN_BITS -
+		   (log (a) / log (2))) * (1 << APU_LOG_BITS));
       apu_lineartbl[i] = ua << 1;
-	}
+    }
 }
 
-static UINT32 APU_DivFix(UINT32 p1, UINT32 p2, UINT32 fix)
+static UINT32
+APU_DivFix (UINT32 p1, UINT32 p2, UINT32 fix)
 {
-    UINT32 ret;
-   /* Fix for possible arithmetic exception on absolute 0. */
-   p1 = MAX(p1, 1);
-   p2 = MAX(p2, 1);
-   ret = p1 / p2;
-	p1  = p1 % p2;/* p1 = p1 - p2 * ret; */
-	while (fix--)
+  UINT32 ret;
+  /* Fix for possible arithmetic exception on absolute 0. */
+  p1 = MAX (p1, 1);
+  p2 = MAX (p2, 1);
+  ret = p1 / p2;
+  p1 = p1 % p2;			/* p1 = p1 - p2 * ret; */
+  while (fix--)
+    {
+      p1 += p1;
+      ret += ret;
+      if (p1 >= p2)
 	{
-		p1 += p1;
-		ret += ret;
-		if (p1 >= p2)
-		{
-			p1 -= p2;
-			ret++;
-		}
+	  p1 -= p2;
+	  ret++;
 	}
-	return ret;
+    }
+  return ret;
 }
 
-#endif   /* !APU_SHARED_H_INCLUDED */
+#endif /* !APU_SHARED_H_INCLUDED */
