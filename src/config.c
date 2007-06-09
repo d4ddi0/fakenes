0b6a0b2fb42f9f8ce8496517ae616c1df913ae71
randilyn <randilyn>
2007-06-09 20:25:19 +0000

    Changes the configuration loading routines so that outdated (and possibly incompatible) configuration files are now handled in a proper way, which prevents many 'bugs' when upgrading which were caused by user error (i.e simply not deleting their old configuration file despite recommendations to do so).

--- src/config.c
+++ src/config.c
@@ -15,28 +15,58 @@
 #include "input.h"
 #include "main.h"
 #include "rewind.h"
+#include "version.h"
 #include "video.h"
 
-void load_config (void)
+void actually_load_config(void)
 {
-   apu_load_config ();
-   audio_load_config ();
-   dsp_load_config ();
-   gui_load_config ();
-   input_load_config ();
-   main_load_config ();
-   rewind_load_config ();
-   video_load_config ();
+   apu_load_config();
+   audio_load_config();
+   dsp_load_config();
+   gui_load_config();
+   input_load_config();
+   main_load_config();
+   rewind_load_config();
+   video_load_config();
 }
 
-void save_config (void)
+void load_config(void)
 {
-   apu_save_config ();
-   audio_save_config ();
-   dsp_save_config ();
-   gui_save_config ();
-   input_save_config ();
-   main_save_config ();
-   rewind_save_config ();
-   video_save_config ();
+   const char* version = get_config_string("meta", "version", "default");
+   if(strcmp(version, VERSION_STRING) != 0) {
+      /* Version mismatch, or version string missing - start with a fresh config file. */
+      push_config_state();
+
+      /* Just set to a nonexistant file so that defaults are always loaded. */
+      /* An empty string is good for this purpose because unlike null, it's a valid string, but it is also an invalid
+        filename and thus cannot exist on any system. */
+      set_config_file("");
+      actually_load_config();
+
+      pop_config_state ();
+
+      /* Write the newly loaded defaults out to disk. */
+      save_config();
+
+      /* Skip the rest. */
+      return;
+   }
+
+   actually_load_config();
+}
+
+void save_config(void)
+{
+   set_config_string("meta", "version", VERSION_STRING);
+
+   apu_save_config();
+   audio_save_config();
+   dsp_save_config();
+   gui_save_config();
+   input_save_config();
+   main_save_config();
+   rewind_save_config();
+   video_save_config();
+
+   flush_config_file();
 }
