1bddf4bf74bbd2b4aa7094f6de16187b40b9fcff
kaedeko <kaedeko@a2471fd5-8713-0410-a344-bafe66df32c7>
2011-07-09 04:08:39 +0000

    Initial work on new file handling routines meant to eventually replace Allegro's mass PACKFILE usage in the emulator, especially in state saving.
    git-svn-id: https://svn.code.sf.net/p/fakenes/code@70 a2471fd5-8713-0410-a344-bafe66df32c7

---
+++ Sources/Toolkit/File.cpp
@@ -0,0 +1,366 @@
+/* FakeNES - A portable, Open Source NES emulator.
+   Copyright © 2011 Digital Carat
+
+   This is free software. See 'License.txt' for additional copyright and
+   licensing information. You must read and accept the license prior to
+   any modification or use of this software. */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include "Common/Global.h"
+#include "Common/Debug.h"
+#include "Common/Types.h"
+#include "File.h"
+
+namespace {
+
+// Determine the native byte order.
+#ifdef LSB_FIRST
+   // Use little-endian byte ordering.
+   const FILE_ORDER NativeOrder = FILE_ORDER_INTEL;
+#else
+   // Use big-endian byte ordering.
+   const FILE_ORDER NativeOrder = FILE_ORDER_MOTOROLA;
+#endif
+
+// This is how much the data buffer grows at a time during writing.
+const FILE_SIZE ChunkSize = 4096;
+
+} // namespace anonymous
+
+// --------------------------------------------------------------------------------
+
+FILE_CONTEXT* open_file(const UCHAR* filename, const FILE_MODE mode, const FILE_ORDER order)
+{
+   Safeguard(filename);
+
+   FILE_CONTEXT* file = malloc(sizeof(FILE_CONTEXT));
+   if(!file) {
+      Warning("Out of memory.");
+      return NULL;
+   }
+
+   memset(file, 0, sizeof(FILE_CONTEXT));
+
+   file->type = FILE_TYPE_PHYSICAL;
+   file->mode = mode;
+   file->order = order;
+
+   const char* access;
+   if(mode == FILE_MODE_READ) {
+      access = "r";
+   } else if(mode == FILE_MODE_WRITE) {
+      access = "w":
+   } else {
+      GenericWarning();
+      return NULL;
+   }
+
+   file->handle = fopen(filename, access);
+   if(!file->handle) {
+      free(file);
+      return NULL;
+   }
+
+   return file;
+}
+
+FILE_CONTEXT* open_memory_file(const FILE_MODE mode, const FILE_ORDER order)
+{
+   FILE_CONTEXT* file = malloc(sizeof(FILE_CONTEXT));
+   if(!file) {
+      Warning("Out of memory.");
+      return NULL;
+   }
+
+   memset(file, 0, sizeof(FILE_CONTEXT));
+
+   file->type = FILE_TYPE_VIRTUAL;
+   file->mode = mode;
+   file->order = order;
+
+   file->buffer.data = malloc(ChunkSize);
+   if(!file->buffer.data) {
+      Warning("Out of memory.");
+      free(file);
+
+      return NULL;
+   }
+
+   file->buffer.limit = ChunkSize;
+
+   return file;
+}
+
+void* get_file_buffer(FILE_CONTEXT* file, FILE_SIZE* size)
+{
+   Safeguard(file);
+
+   if(file->type == FILE_TYPE_PHYSICAL) {
+      Warning("This is not a memory file.");
+      return NULL;
+   }
+
+   if(size)
+      *size = file->buffer.size;
+
+   return file->buffer.data;
+}
+
+void set_file_buffer(FILE_CONTEXT* file, void* data, const FILE_SIZE size)
+{
+   Safeguard(file);
+   Safeguard(data);
+   Safeguard(size > 0);
+
+   if(file->type == FILE_TYPE_PHYSICAL) {
+      Warning("This is not a memory file.");
+      return;
+   }
+
+   if(file->mode == FILE_MODE_WRITE) {
+      Warning("This action is not valid in write mode.");
+      return;
+   }
+
+   file->buffer.data = data;
+   file->buffer.size = size;
+   file->buffer.limit = size;
+   file->buffer.position = 0;
+}
+
+// --------------------------------------------------------------------------------
+
+static forceinline uint16 SwapWord(const uint16 data, FILE_ORDER order)
+{
+   // We only need to swap when a mismatch is detected.
+   if((order == FILE_ORDER_NATIVE) || (order == NativeOrder))
+      return data;
+
+   const uint8 lower = data & 0x00FF;
+   const uint8 upper = (data & 0xFF00) >> 8;
+   return (lower << 8) | upper;
+}
+
+static forceinline uint32 SwapLong(const uint32 data, FILE_ORDER order)
+{
+   if((order == FILE_ORDER_NATIVE) || (order == NativeOrder))
+      return data;
+
+   const uint16 lower = data & 0x0000FFFF;
+   const uint16 upper = (data & 0xFFFF0000) >> 16;
+   return (SwapWord(upper) << 16) | SwapWord(lower);
+}
+
+static FILE_SIZE File_Read(FILE_CONTEXT* file, void* data, const FILE_SIZE size)
+{
+   Safeguard(file);
+   Safeguard(data);
+
+   // Verify that the file is opened in read mode.
+   if(file->mode != FILE_MODE_READ) {
+      GenericWarning();
+      return 0;
+   }
+
+   if(size == 0) {
+      // Nothing to do.
+      return 0;
+   }
+
+   if(file->type == FILE_TYPE_PHYSICAL) {
+      if(!file->handle) {
+         GenericWarning();
+         return 0;
+      }
+
+      return fread(data, size, 1, file->handle);
+   }
+
+   FILE_BUFFER& buffer = file->buffer;
+   if(!buffer.data) {
+      GenericWarning();
+      return 0;
+   }
+
+   const FILE_SIZE start = buffer.offset;
+   const FILE_SIZE end = Clamp<FILE_SIZE>(buffer.offset + size, 0, buffer.size - 1);
+   const FILE_SIZE count = end - start;
+
+   memcpy(data, buffer.data + start, count);
+   return count;
+}
+
+static FILE_SIZE File_Write(FILE_CONTEXT* file, const void* data, const FILE_SIZE size)
+{
+   Safeguard(file);
+   Safeguard(data);
+
+   if(file->mode != FILE_MODE_WRITE) {
+      GenericWarning();
+      return 0;
+   }
+
+   if(size == 0) {
+      // Nothing to do.
+      return 0;
+   }
+   
+   if(file->type == FILE_TYPE_PHYSICAL) {
+      if(!file->handle) {
+         GenericWarning();
+         return 0;
+      }
+
+      return fwrite(data, size, 1, file->handle);
+   }
+
+   FILE_BUFFER& buffer = file->buffer;
+   if(!buffer.data) {
+      GenericWarning();
+      return 0;
+   }
+
+   const FILE_SIZE end = buffer.offset + size;
+   if(end > buffer.limit) {
+      FILE_SIZE resize = 0;
+      while(resize < end)
+         resize += ChunkSize;
+
+      buffer.data = realloc(buffer.data, resize);
+      if(!buffer.data)
+         GenericWarning();
+
+      buffer.limit = resize;
+   }
+
+   memcpy(buffer.data + buffer.offset, data, size);
+   buffer.offset += size;
+
+   // If we passed the end of the file, we have to adjust the size.
+   if(buffer.offset > buffer.size)
+      buffer.size = buffer.offset;
+
+   return size;
+}
+
+static void File_SeekFrom(FILE_CONTEXT* file, const FILE_OFFSET offset)
+{
+   Safeguard(file);
+}
+
+static void File_SeekTo(FILE_CONTEXT* file, const FILE_SIZE position)
+{
+   Safeguard(file);
+}
+
+static void File_Flush(FILE_CONTEXT* file)
+{
+   Safeguard(file);
+
+   if(file->type != FILE_TYPE_PHYSICAL)
+      return;
+
+   if(!file->handle) {
+      WARN_GENERIC();
+      return;
+   }
+
+   fflush(file->handle);
+}
+
+static void File_Close(FILE_CONTEXT* file)
+{
+   Safeguard(file);
+
+   if(file->type == FILE_TYPE_PHYSICAL) {
+      fclose(file->handle);
+   }
+
+   free(file);
+}
+
+static UINT8 File_GetByte(FILE_CONTEXT* file)
+{
+   Safeguard(file);
+
+   UINT8 data;
+   file->read(file, &data, sizeof(data));
+
+   return data;
+}
+
+static UINT16 File_GetWord(FILE_CONTEXT* file)
+{
+   Safeguard(file);
+
+   UINT16 data;
+   file->read(file, &data, sizeof(data));
+
+   return SwapWord(data, file->order);
+}
+
+static UINT32 File_GetLong(FILE_CONTEXT* file)
+{
+   Safeguard(file);
+
+   UINT32 data;
+   file->read(file, &data, sizeof(data));
+
+   return SwapLong(data, file->order);
+}
+
+static BOOL File_GetBoolean(FILE_CONTEXT* file)
+{
+   Safeguard(file);
+
+   const uint8 data = file->get_byte(file);
+   return TRUE_OR_FALSE(data);
+}
+
+static REAL File_GetReal(FILE_CONTEXT* file)
+{
+   const uint32 data = file->get_long(file);
+   const float* data_ptr = (float*)&data;
+   return *data_ptr;
+}
+
+static void File_PutByte(FILE_CONTEXT* file, const UINT8 data)
+{
+   Safeguard(file);
+
+   file->write(file, &data, sizeof(data));
+}
+
+static void File_PutWord(FILE_CONTEXT* file, const UINT16 data)
+{
+   Safeguard(file);
+
+   const uint16 swapped = SwapWord(data, file->order);
+   file->write(file, &swapped, sizeof(swapped));
+}
+
+static void File_PutLong(FILE_CONTEXT* file, const UINT32 data)
+{
+   Safeguard(file);
+
+   const uint32 swapped = SwapLong(data, file->order);
+   file->write(file, &swapped, sizeof(swapped));
+}
+
+static void File_PutBoolean(FILE_CONTEXT* file, const BOOL data)
+{
+   Safeguard(file);
+
+   const uint8 value = ZERO_OR_ONE(data);
+   file->put_byte(file, value);
+}
+
+static void File_PutReal(FILE_CONTEXT* file, const REAL data)
+{
+   Safeguard(file);
+
+   const uint32* data_ptr = (uint32*)&data;
+   file->put_long(file, *data_ptr);
+}
---
+++ Sources/Toolkit/File.h
@@ -0,0 +1,102 @@
+/* FakeNES - A portable, Open Source NES emulator.
+   Copyright © 2011 Digital Carat
+
+   This is free software. See 'License.txt' for additional copyright and
+   licensing information. You must read and accept the license prior to
+   any modification or use of this software. */
+
+#ifndef TOOLKIT__FILE_H__INCLUDED
+#define TOOLKIT__FILE_H__INCLUDED
+#include <stdio.h>
+#include "Common/Global.h"
+#include "Common/Types.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* For physical files, the file routines simply act as a layer on top of
+   the C library file I/O routines, and the 'buffer' field of the file
+   context is never used for anything.
+
+   For reading memory files, the data buffer must first be set to an
+   existing block of memory via a call to set_file_buffer(). 
+
+   For writing memory files, the data buffer is automatically allocated
+   and managed accordingly. As data is added to the buffer, it will grow
+   by the chunk size to accomodate the new data. You can retrieve the
+   data buffer via a call to get_file_buffer(). */
+enum FILE_TYPE {
+   FILE_TYPE_PHYSICAL = 0,	/* Physical file. */
+   FILE_TYPE_VIRTUAL		/* Memory file. */
+};
+
+/* Note that no duplex mode is supported yet. */
+enum FILE_MODE {
+   FILE_MODE_READ = 0,		/* Open for reading. */
+   FILE_MODE_WRITE,		/* Open for writing. */
+};
+
+enum FILE_ORDER {
+   FILE_ORDER_NATIVE = 0,	/* Auto-detect native byte order. */
+   FILE_ORDER_INTEL,		/* Always little-endian. */
+   FILE_ORDER_MOTOROLA,		/* Always big-endian. */
+};
+
+/* Data type used for sizes, positions, etc. */
+typedef UINT64 FILE_SIZE;
+/* Data type used for relative offsets. */
+typedef INT64 FILE_OFFSET;
+
+typedef struct _FILE_BUFFER {
+   FILE_SIZE size, limit;
+   FILE_SIZE position;
+   void* data;
+
+} FILE_BUFFER;
+
+typedef struct _FILE_CONTEXT {
+   FILE_TYPE type;	/* File type. See above.*/
+   FILE_MODE mode;	/* File access mode. See above. */
+   FILE_ORDER order;	/* File byte order. See above. */
+   FILE_BUFFER buffer;	/* File data buffer and related parameters. */
+
+   /* General functions. These can be overriden by replacing the file
+      pointers, allowing custom routines to be used for a given file
+      without changing any code that accesses it. */
+   FILE_SIZE (*read)(FILE_CONTEXT* file, void* data, const FILE_SIZE size);
+   FILE_SIZE (*write)(FILE_CONTEXT* file, const void* data, const FILE_SIZE size);
+   void (*seek_from)(FILE_CONTEXT* file, const FILE_OFFSET offset);
+   void (*seek_to)(FILE_CONTEXT* file, const FILE_SIZE position);
+   void (*flush)(FILE_CONTEXT* file);
+   void (*close)(FILE_CONTEXT* file);
+
+   /* Data access functions. Most of these are subject to endianness.
+      Note that get_real() and put_real() are reduced to single precision,
+      and booleans require an entire byte of storage space. Again, these
+      can be overriden on a per-file basis.*/
+   UINT8 (*get_byte)(FILE_CONTEXT* file);
+   UINT16 (*get_word)(FILE_CONTEXT* file);
+   UINT32 (*get_long)(FILE_CONTEXT* file);
+   BOOL (*get_boolean)(FILE_CONTEXT* file);
+   REAL (*get_real)(FILE_CONTEXT* file, );
+   void (*put_byte)(FILE_CONTEXT* file, const UINT8 data);
+   void (*put_word)(FILE_CONTEXT* file, const UINT16 data);
+   void (*put_long)(FILE_CONTEXT* file, const UINT32 data);
+   void (*put_boolean)(FILE_CONTEXT* file, const BOOL data);
+   void (*put_real)(FILE_CONTEXT* file, const REAL data);
+
+   /* Underlying file handle in the C library. This is only used for
+      physical files, not memory files. */
+   FILE* handle;
+
+} FILE_CONTEXT;
+
+extern FILE_CONTEXT* open_file(const UCHAR* filename, const FILE_MODE mode, const FILE_ORDER order);
+extern FILE_CONTEXT* open_memory_file(const FILE_MODE mode, const FILE_ORDER order);
+extern void* get_file_buffer(FILE_CONTEXT* file, FILE_SIZE* size);
+extern void set_file_buffer(FILE_CONTEXT* file, void* buffer, const FILE_SIZE size);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* TOOLKIT__FILE_H__INCLUDED */
