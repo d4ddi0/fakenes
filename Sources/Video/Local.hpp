2fa01dbb515356387258783f5f31f32786a9efae
kaedeko <kaedeko@a2471fd5-8713-0410-a344-bafe66df32c7>
2011-07-13 02:57:02 +0000

    Initial cleanup of header file list (still incomplete).
    git-svn-id: https://svn.code.sf.net/p/fakenes/code@115 a2471fd5-8713-0410-a344-bafe66df32c7

---
+++ Sources/Video/Local.hpp
@@ -0,0 +1,39 @@
+/* FakeNES - A portable, Open Source NES emulator.
+   Copyright © 2011 Digital Carat
+
+   This is free software. See 'License.txt' for additional copyright and
+   licensing information. You must read and accept the license prior to
+   any modification or use of this software. */
+
+#ifndef Video__Local_hpp__included
+#define Video__Local_hpp__included
+#include <allegro.h>
+#ifdef USE_ALLEGROGL
+#include <alleggl.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include "Common/Binary.h"
+#include "Common/Debug.h"
+#include "Common/Global.h"
+#include "Common/Math.h"
+#include "Common/Types.h"
+#include "Audio/APU.h"
+#include "Audio/Audio.h"
+#include "Core/CPU.h"
+#include "GUI/GUI.h"
+#include "Platform/Datafile.h"
+#include "Platform/Load.h"
+#include "Platform/Log.h"
+#include "System/Input.h"
+#include "System/Mapper.h"
+#include "System/Machine.h"
+#include "System/Timing.h"
+
+#endif // !Video__Local_hpp__included
