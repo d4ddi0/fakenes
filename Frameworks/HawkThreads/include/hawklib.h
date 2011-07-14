fffe2edb118c9a27725535e9d3db922b5ac50b35
kaedeko <kaedeko@a2471fd5-8713-0410-a344-bafe66df32c7>
2011-07-14 22:54:32 +0000

    Attempting to fix build errors on Windows.
    git-svn-id: https://svn.code.sf.net/p/fakenes/code@148 a2471fd5-8713-0410-a344-bafe66df32c7

--- Frameworks/HawkThreads/include/hawklib.h
+++ Frameworks/HawkThreads/include/hawklib.h
@@ -23,7 +23,7 @@
 #ifndef HAWKLIB_H
 #define HAWKLIB_H
 
-#if defined(WINDOWS) || defined (WIN32) || defined (WIN64) || defined (_WIN32_WCE)
+#if defined(SYSTEM_WINDOWS) || defined (WIN32) || defined (WIN64) || defined (_WIN32_WCE)
 #define HL_WINDOWS_APP
 #endif
 
