88890473c137cb11bad24a4e0e7e9ec826ecb032
kaedeko <kaedeko@a2471fd5-8713-0410-a344-bafe66df32c7>
2011-07-15 01:10:53 +0000

    Added a missing file.
    git-svn-id: https://svn.code.sf.net/p/fakenes/code@154 a2471fd5-8713-0410-a344-bafe66df32c7

---
+++ Sources/Platform/Local.hpp
@@ -0,0 +1,26 @@
+/* FakeNES - A portable, Open Source NES emulator.
+   Copyright © 2011 Digital Carat
+
+   This is free software. See 'License.txt' for additional copyright and
+   licensing information. You must read and accept the license prior to
+   any modification or use of this software. */
+
+#ifndef Platform__Local_hpp__included
+#define Platform__Local_hpp__included
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include "Audio/APU.h"
+#include "Common/Global.h"
+#include "Common/Inline.h"
+#include "Common/Debug.h"
+#include "Common/Types.h"
+#include "Core/CPU.h"
+#include "System/Input.h"
+#include "System/Machine.h"
+#include "System/Mapper.h"
+#include "System/ROM.h"
+#include "System/Timing.h"
+#include "Video/PPU.h"
+
+#endif // !Platform__Local_hpp__included
