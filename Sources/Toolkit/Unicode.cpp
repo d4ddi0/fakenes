74bfa0f87de079691f908647cb4f07773bb0b991
kaedeko <kaedeko@a2471fd5-8713-0410-a344-bafe66df32c7>
2012-06-11 21:03:58 +0000

    Added proper support for UTF8-CPP to the build system, which can now be accessed by #include <utf8.h> from source files.
    git-svn-id: https://svn.code.sf.net/p/fakenes/code@186 a2471fd5-8713-0410-a344-bafe66df32c7

--- Sources/Toolkit/Unicode.cpp
+++ Sources/Toolkit/Unicode.cpp
@@ -5,6 +5,7 @@
    licensing information. You must read and accept the license prior to
    any modification or use of this software. */
 
+#include <utf8.h> // UTF8-CPP
 #include <cctype>
 #include <cstdlib>
 #include <cstring>
